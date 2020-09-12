c48a3cc283e0a9f1949ca6c66e122c8a98b9aa0b
romain.chor <romain.chor@yahoo.fr>
2020-09-12 11:42:21 +0200

    Updated version

--- toolbox.py
+++ toolbox.py
@@ -3,26 +3,27 @@ Machine learning helper toolbox
 Author: Romain Chor
 
 Libraries:
-numpy
-pandas
-matplotlib.pyplot
-seaborn
-sklearn.model_selection, sklearn.metrics
+	numpy
+	pandas
+	matplotlib.pyplot
+	seaborn
+	sklearn.model_selection, sklearn.metrics
 
 Functions:
-missing_val
-nan_filling
-rmse
-filling_season
-train_test_base
-train_test_random
-train_test_cv
-plot_confusion_matrix
-display_side_by_side
-extract_json
+	missing_val
+	nan_filling
+	rmse
+	mape
+	filling_season
+	train_test_base
+	train_test_random
+	train_test_cv
+	plot_confusion_matrix
+	display_side_by_side
+	extract_json
 
 Classes:
-Blender
+	Blender
 """
 
 print(__doc__)
@@ -73,6 +74,11 @@ def nan_filling(x):
 def rmse(targets, predictions):
 	"""Calculates RMSE between predicitions and ground truth targets"""
 	return np.sqrt(np.mean((predictions - targets) ** 2))
+
+
+def mape(targets, predictions):
+	"""Calculates MAPE between predicitions and ground truth targets"""
+	return np.mean(np.abs((predictions - targets)/targets))*100
 
 
 def filling_season(data):
@@ -226,6 +232,7 @@ class Blender():
 			    y_pred += model.predict_proba(X)[:, 1]
 			else:
 				y_pred += model.predict(X)
+		y_pred /= len(self.models)
 
 		return y_pred
 
